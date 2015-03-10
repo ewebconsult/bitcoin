ccf82d2bf9a1d5339afe10f69ade3374a8d2f8d4
sstone <samuel.stone@hotmail.co.za>
2015-03-10 21:51:58 +0100

    add bitcoin-config.h

---
+++ src/config/bitcoin-config.h
@@ -0,0 +1,26 @@
+#ifndef __BITCOIN_CONFIG_H__
+#define __BITCOIN_CONFIG_H__
+
+#define CLIENT_VERSION_MAJOR 0
+#define CLIENT_VERSION_MINOR 10
+#define CLIENT_VERSION_REVISION 99
+#define CLIENT_VERSION_BUILD 0
+
+//! Set to true for release, false for prerelease or test build
+#define CLIENT_VERSION_IS_RELEASE false
+
+/**
+* Copyright year (2009-this)
+* Todo: update this when changing our copyright comments in the source
+*/
+#define COPYRIGHT_YEAR 2015
+
+#define HAVE_WORKING_BOOST_SLEEP_FOR
+
+#if defined(_WIN64)
+typedef __int64 ssize_t;
+#else
+typedef long ssize_t;
+#endif
+
+#endif  // __BITCOIN_CONFIG_H__
